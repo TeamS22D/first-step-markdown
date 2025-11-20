ddb078baa4f2c38ebe774f03d0ae5f2d693109c7
민선영 <ttt4306@gmail.com>
2025-11-20 15:49:14 +0900

    파일 구조 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/Markdown/Markdown.tsx'
+import App from './pages/Mission/Commitment/Document/components/Markdown/Markdown.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
